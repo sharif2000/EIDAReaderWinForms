a6f10c38ab5c174df44507dafc41d54c041a8b5b
Sherif Abdellatif <sharif_pc@hotmail.com>
2017-12-18 10:15:21 +0400

    Opened and Closed EIDA "Context"

--- HelloEIDA/Form1.cs
+++ HelloEIDA/Form1.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
+using EmiratesId.AE;
+using EmiratesId.AE.Exceptions;
+using EmiratesId.AE.ReadersMgt;
 
 namespace HelloEIDA
 {
@@ -15,6 +10,21 @@ namespace HelloEIDA
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            try
+            {
+                ReaderManagement readerMgr = new ReaderManagement();
+                readerMgr.EstablishContext();
+                // ... do some ID Card operations ...
+                readerMgr.CloseContext();
+            }
+            catch (MiddlewareException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
